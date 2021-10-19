5ecf3e996e7ea547231b1517d033dff6d752a4fe
maxi11037 <64989228+maxi11037@users.noreply.github.com>
2021-10-19 18:08:00 -0300

    Cambios en el landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,15 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
+    path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule)
+  },
+  {
     path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
   },
   {
     path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
   },
-  {
-    path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule)
-  },
+  
   {
     path: 'invitar-jugadores', loadChildren: () => import('./pages/invitar-jugadores/invitar-jugadores.module').then(m => m.InvitarJugadoresModule)
   },
